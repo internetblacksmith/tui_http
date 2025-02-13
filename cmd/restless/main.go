89827e957318431266b943ee230bd99434f095c3
Paolo Fabbri <jabawack81@gmail.com>
2025-02-13 21:41:23 +0000

    feat: add main entry point with Bubble Tea program setup
    Wire up the TUI application bootstrap in cmd/restless using
    tea.NewProgram with alternate screen mode.

---
+++ cmd/restless/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"restless/pkg/ui"
+)
+
+func main() {
+	model := ui.NewModel()
+
+	p := tea.NewProgram(model, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
